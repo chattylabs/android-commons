01170c62624e10712ccad0e14a4d3e53c50b954a
Francisco Riadigos <riadigos.fran@gmail.com>
2018-07-15 21:18:31 +0100

    [patch] Get any metadata strings by key.

--- sdk-common/src/main/java/com/chattylabs/sdk/android/common/ManifestUtils.java
+++ sdk-common/src/main/java/com/chattylabs/sdk/android/common/ManifestUtils.java
@@ -8,10 +8,22 @@ public class ManifestUtils {
     private static final String APP_ID = "com.chattylabs.sdk.APP_ID";
 
     public static String getAppId(Context context) {
-        return getManifestString(context, APP_ID);
+        return getMetaDataString(context, APP_ID);
     }
 
+    /**
+     * Returns a string stored in a metadata tag of the manifest file.
+     * @deprecated use {@link #getMetaDataString(Context, String)} instead.
+     */
+    @Deprecated
     public static String getManifestString(Context context, String key) {
+        return getMetaDataString(context, key);
+    }
+
+    /**
+     * Returns a string stored in a metadata tag of the manifest file.
+     */
+    public static String getMetaDataString(Context context, String key) {
         return getBundle(context).getString(key);
     }
 
