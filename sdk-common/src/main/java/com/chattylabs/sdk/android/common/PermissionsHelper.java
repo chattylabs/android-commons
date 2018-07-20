d9aa171a47d4477aa0fbacd275613eb9ea263215
Francisco Riadigos <riadigos.fran@gmail.com>
2018-07-20 16:36:58 +0100

    [patch] Updated PermissionsHelper to add some utils for demos

--- sdk-common/src/main/java/com/chattylabs/sdk/android/common/PermissionsHelper.java
+++ sdk-common/src/main/java/com/chattylabs/sdk/android/common/PermissionsHelper.java
@@ -3,18 +3,21 @@ package com.chattylabs.sdk.android.common;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
 public abstract class PermissionsHelper {
 
-    public static void check(Activity activity, String[] permissions) {
+    public static final int REQUEST_CODE = 100010;
+
+    public static void check(Activity activity, String[] permissions, Runnable runnable) {
         if (needsPermissions(activity.getApplicationContext(), permissions)) {
 
             // Should we show an explanation?
             if (needsExplanation(activity, permissions)) {
 
-                ActivityCompat.requestPermissions(activity, permissions, 10);
+                ActivityCompat.requestPermissions(activity, permissions, REQUEST_CODE);
 
                 // TODO
                 // Show an explanation to the user *asynchronously* -- don't block
@@ -26,15 +29,25 @@ public abstract class PermissionsHelper {
 
                 ActivityCompat.requestPermissions(activity, permissions, 10);
             }
+        } else {
+            runnable.run();
         }
     }
 
-    private static boolean needsPermissions(Context context, String[] permissions) {
+    public static boolean needsPermissions(Context context, String[] permissions) {
         for (String permission : permissions) {
             if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED)
                 return true;
         }
         return false;
+    }
+
+    public  static boolean isPermissionGranted(@NonNull int[] grantResults) {
+        return grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+    }
+
+    public static boolean isPermissionRequest(int requestCode) {
+        return requestCode == REQUEST_CODE;
     }
 
     private static boolean needsExplanation(Activity activity, String[] permissions) {
