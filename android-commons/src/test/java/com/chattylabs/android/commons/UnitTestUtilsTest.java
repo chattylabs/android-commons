7ca50bc623cc81a1209409fe74d211345e0285eb
Francisco Riadigos <riadigos.fran@gmail.com>
2019-11-12 16:06:46 +0000

    [feature] Updating SerialThread to Blocking

---
+++ android-commons/src/test/java/com/chattylabs/android/commons/UnitTestUtilsTest.java
@@ -0,0 +1,12 @@
+package com.chattylabs.android.commons;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTestUtilsTest {
+
+    @Test
+    public void setFinalStatic() {
+    }
+}
